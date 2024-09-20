f1f82b7dee67bb82cda3d684d2c1ad664e72b71d
janaxhbl <janahaibl@gmail.com>
2024-09-20 16:05:00 +0200

    add primevue and remove boilerplate

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,37 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primevue/themes'
+
+const MyPreset = definePreset(Aura, {
+  //Your customizations, see the following sections for examples
+  semantic: {
+    primary: {
+      50: '{purple.50}',
+      100: '{purple.100}',
+      200: '{purple.200}',
+      300: '{purple.300}',
+      400: '{purple.400}',
+      500: '{purple.500}',
+      600: '{purple.600}',
+      700: '{purple.700}',
+      800: '{purple.800}',
+      900: '{purple.900}',
+      950: '{purple.950}'
+    }
+  }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset
+  }
+})
 
 app.mount('#app')
