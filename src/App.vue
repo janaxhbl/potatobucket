3d7ed280c11a3306cdcd4bc62521c65a566dfe1e
janaxhbl <janahaibl@gmail.com>
2024-09-28 12:04:43 +0200

    try to fix scrollbug on mobile11

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ const breakpoint = ref("200px");
 <template>
   <div
     id="main"
-    style="height: 100dvh; width: 100dvw"
+    style="height: 100svh; width: 100vw"
     class="bg-[#EBE0BD] flex flex-col"
   >
     <div id="routerview" class="h-full p-4 overflow-auto">
