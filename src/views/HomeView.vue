f716a05c48de0dd3b6be58b8a34e41d2f7d7840c
janaxhbl <janahaibl@gmail.com>
2024-10-03 17:49:45 +0200

    implement editing gametypes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,6 +19,9 @@ let newGameTypeListOption = ref([{ name: "neue Spielart hinzufügen" }]);
 let showCreateGameTypePopup: Ref<boolean> = ref(false);
 let newGameType: Ref<GameType> = ref({} as GameType);
 
+let showEditGameTypePopup: Ref<boolean> = ref(false);
+let selectedGameType: Ref<GameType> = ref({} as GameType);
+
 async function createNewGameType() {
   if (
     newGameType.value.name == null ||
@@ -56,6 +59,29 @@ async function createNewGameType() {
   getGameTypes();
 }
 
+function openEditGameTypePopup() {
+  showEditGameTypePopup.value = true;
+  selectedGameType.value.countType =
+    selectedGameType.value.countType == "plus" ? "Rauf" : "Runter";
+}
+
+async function editGameType() {
+  selectedGameType.value.countType =
+    selectedGameType.value.countType == "Rauf" ? "plus" : "minus";
+
+  let index_to_splice = gameTypes.value.findIndex(
+    (obj) => obj.id == selectedGameType.value.id
+  );
+  gameTypes.value.splice(index_to_splice, 1, selectedGameType.value);
+
+  await FirestoreDB.updateDocument("game_types", "game_types", {
+    game_types: gameTypes.value,
+  });
+
+  showEditGameTypePopup.value = false;
+  selectedGameType.value = {} as GameType;
+}
+
 async function getGameTypes() {
   let data = FirestoreDB.getAllInCollection("game_types");
   data.then((value) => {
@@ -91,12 +117,14 @@ onMounted(() => {
                 @change="showCreateGameTypePopup = true"
               />
               <Listbox
+                v-model="selectedGameType"
                 :options="gameTypes"
                 option-label="name"
                 class="overflow-y-hidden py-1"
                 :scroll-height="'100%'"
                 :striped="true"
                 empty-message="keine Spielarten vorhanden"
+                @change="openEditGameTypePopup"
               />
             </div>
           </AccordionContent>
@@ -161,6 +189,68 @@ onMounted(() => {
             type="button"
             label="Erstellen"
             @click="createNewGameType"
+          ></Button>
+        </div>
+      </div>
+    </Dialog>
+
+    <Dialog
+      v-model:visible="showEditGameTypePopup"
+      modal
+      header="Spielart bearbeiten"
+      :style="{ width: '80vw' }"
+    >
+      <div class="flex flex-col">
+        <div class="flex flex-row mb-4">
+          <div id="links" class="flex flex-col justify-between py-2 w-1/3">
+            <label for="name" class="font-semibold">Name</label>
+            <label for="startValue" class="font-semibold">Startwert</label>
+            <label for="endValue" class="font-semibold">Endwert</label>
+          </div>
+          <div id="rechts" class="flex flex-col w-2/3 gap-4">
+            <InputText
+              id="name"
+              v-model="selectedGameType.name"
+              fluid
+              class="w-2/3"
+              autocomplete="off"
+            />
+            <InputNumber
+              id="startValue"
+              v-model="selectedGameType.startValue"
+              fluid
+              input-class="w-2/3"
+              autocomplete="off"
+            />
+            <InputNumber
+              id="endValue"
+              v-model="selectedGameType.endValue"
+              fluid
+              input-class="w-2/3"
+              autocomplete="off"
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-row">
+          <SelectButton
+            v-model="selectedGameType.countType"
+            :options="['Rauf', 'Runter']"
+            class="mb-4 flex w-full"
+          />
+        </div>
+        <div class="flex justify-between gap-2">
+          <Button
+            type="button"
+            label="Abbrechen"
+            severity="secondary"
+            @click="showEditGameTypePopup = false"
+          ></Button>
+          <Button
+            raised
+            type="button"
+            label="Speichern"
+            @click="editGameType"
           ></Button>
         </div>
       </div>
