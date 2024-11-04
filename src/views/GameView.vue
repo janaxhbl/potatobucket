d77feff8c4a3a6b6207d673155afa111f600be09
janaxhbl <janahaibl@gmail.com>
2024-11-04 18:31:46 +0100

    fix toggleBummerl on mobile

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -443,7 +443,11 @@ onUpdated(() => {
             :header-class="setHeaderPaddingClass()"
           >
             <template #header>
-              <div class="flex flex-col" @click="toggleBummerl">
+              <div
+                class="flex flex-col"
+                @click="toggleBummerl"
+                @touchend="toggleBummerl"
+              >
                 <span :class="setHeaderClass(i - 1)">
                   {{ players[i - 1].header }}
                 </span>
